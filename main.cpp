234d4bac1b014d35dd01f5aaad70ef6739f4d1cf
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2022-06-12 12:30:21 -0400

    Cleared practice code

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 #include <thread>
 
-void function1(char symbol)
-{
-    for(int i = 0; i < 200; i++)
-        std::cout << symbol;
-    std::cout << std::endl;
-}
-
-void function2()
-{
-    for(int i = 0; i < 200; i++)
-        std::cout << "-";
-    std::cout << std::endl;
-}
-
 int main()
 {
-    std::thread worker1(function1, 'o');
-    std::thread worker2(function2);
 
-    worker1.join();
-    worker2.join();
 
     return 0;
 }
