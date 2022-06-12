cb0d998797530956b6830d9a9cd89f5c0cd4a7ac
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2022-06-12 12:07:51 -0400

    Singlethreaded example added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <thread>
 
+void function1()
+{
+    for(int i = 0; i < 200; i++)
+        std::cout << "+";
+    std::cout << std::endl;
+}
+
+void function2()
+{
+    for(int i = 0; i < 200; i++)
+        std::cout << "-";
+    std::cout << std::endl;
+}
+
 int main()
 {
-
+    function1();
+    function2();
 
     return 0;
 }
